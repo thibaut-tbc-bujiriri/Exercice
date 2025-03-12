fb3f4d3888e30388100924f81679d3be640a8259
Thibaut <thibauttbcbujiriri@gmail.com>
2025-03-12 16:52:35 +0100

    changement

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function(){
 
 // alert(" Bienvenu sur ma page de presentation...")
 // console.warn("Attention !!!");
+
+const camerafeed = document.getElementById('camerafeed');
+navigator.mediaDevices.getUserMedia({video: true}).
+then(stream => {
+    camerafeed.srcObject = stream;
+})
+.catch(err => {console.error("erreur d'acces a la camera :",err)});
